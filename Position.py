48e7322b4098cbdc1fe644241affc1bf04f0899c
stryker236 <rri.nascimento@gmail.com>
2022-06-12 22:05:52 +0100

    comentario no positions

--- Position.py
+++ Position.py
@@ -4,6 +4,10 @@ from __future__ import annotations
 class Position:
     _pos = tuple[int, int]
 
+    #----"__init__"---------------------------------------------------------------------------------------------------------
+    #   Creates new instance of Position from a given row and column. Verifies if arguments are float, int and non negative.
+    #   Arguments: row - row of the given position; col - column of the given position.
+    #-----------------------------------------------------------------------------------------------------------------------
     def __init__(self, row: int, col: int):
         if isinstance(row,(float,int)) and isinstance(col,(float,int)):
             if col >= 0 and row >= 0:
@@ -11,25 +15,45 @@ class Position:
         else:
             raise ValueError
 
+    #----"__str__"----------------------------------------------------------------------------------------------------------
+    #   Represents the Position object as a string.
+    #-----------------------------------------------------------------------------------------------------------------------
     def __str__(self):
         return str(self._pos)
 
+    #----"__repr__"---------------------------------------------------------------------------------------------------------
+    #   Represents the Position object as a string.
+    #-----------------------------------------------------------------------------------------------------------------------
     def __repr__(self):
         return self.__str__()
 
+    #----"__hash__"---------------------------------------------------------------------------------------------------------
+    #   Returns the Position object hash value as an integer.
+    #-----------------------------------------------------------------------------------------------------------------------
     def __hash__(self):
         return hash(self._pos)
 
+    #----"__getitem__"------------------------------------------------------------------------------------------------------
+    #   Returns one of the Position object value from a given argument.
+    #   Arguments: item - 0 for row return or 1 for column return.)
+    #-----------------------------------------------------------------------------------------------------------------------
     def __getitem__(self, item: int) -> int:
-        # Nao sei o que acontece se se pedir um indice inválido
         if isinstance(item,int):
             return self._pos[item]
 
+    #----"__eq__"-----------------------------------------------------------------------------------------------------------
+    #   Verifies if two given Positions are equal or not.
+    #   Arguments: Position - Position from which to compare.
+    #-----------------------------------------------------------------------------------------------------------------------
     def __eq__(self, other: Position):
         if isinstance(other,Position):
             return self._pos == other._pos
         return False
     
+    #----"__lt__"-----------------------------------------------------------------------------------------------------------
+    #   Verifies if the object is lesser then a given Position.
+    #   Arguments: other - Position from which to compare.
+    #-----------------------------------------------------------------------------------------------------------------------
     def __lt__(self,other):
         if isinstance(other,Position):
             if self[0] < other[0]:
